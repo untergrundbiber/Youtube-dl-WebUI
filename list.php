c138ed87a9355681cc9d325ff3675639f99c5abd
Florent Kaisser <florent.pro@kaisser.name>
2015-08-16 11:13:10 +0200

    add preview (with html5 video balise) in list of videos

--- list.php
+++ list.php
@@ -1,3 +1,14 @@
+<link href="http://vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
+<script src="http://vjs.zencdn.net/4.12/video.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script>
+function show_video(i) {
+  $("#preview"+i).toggle();
+  $("#preview"+i+"_html5_api").toggle();
+  $("#preview"+i+"_html5_api").get(0).play()
+}
+
+</script>
 <?php
 	require_once 'class/Session.php';
 	require_once 'class/Downloader.php';
@@ -43,8 +54,9 @@
 			<table class="table table-striped table-hover ">
 				<thead>
 					<tr>
-						<th style="min-width:800px; height:35px">Title</th>
+						<th style="min-width:700px; height:35px">Title</th>
 						<th style="min-width:80px">Size</th>
+						<th style="min-width:110px">Download link</th>
 						<th style="min-width:110px">Delete link</th>
 					</tr>
 				</thead>
@@ -55,9 +67,19 @@
 
 				foreach($files as $f)
 				{
+          $video_url = $file->get_downloads_folder().'/'.$f["name"];
 					echo "<tr>";
-					echo "<td><a href=\"".$file->get_downloads_folder().'/'.$f["name"]."\" download>".$f["name"]."</a></td>";
+					echo "<td><a href=\"#\" onclick=\"show_video(".$i.");\" >".$f["name"]."</a>";
+          echo "<video hidden id=\"preview".$i."\" class=\"video-js vjs-default-skin\" controls preload=\"none\"  width=\"320\" height=\"240\"  data-setup='{}'>
+  <source src=\"".$video_url."\" type='video/mp4'>
+  <p class=\"vjs-no-js\">
+    To view this video please enable JavaScript, and consider upgrading to a web browser
+    that <a href=\"http://videojs.com/html5-video-support/\" target=\"_blank\">supports HTML5 video</a>
+  </p>
+</video>";
+          echo "</td>";
 					echo "<td>".$f["size"]."</td>";
+          echo "<td><a href=\"".$video_url."\" download class=\"btn btn-danger btn-sm\">Download</a></td>";
 					echo "<td><a href=\"./list.php?delete=$i&type=$t\" class=\"btn btn-danger btn-sm\">Delete</a></td>";
 					echo "</tr>";
 					$i++;
