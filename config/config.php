ce3daacf377a53ea928c51efddfe7a804b0406fa
ljlabs <jordaan0@gmail.com>
2016-05-10 09:13:10 +0200

    modified:   config/config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 //No "/" at the end of outputFolder
 
 return array(
-	"security" => false,
+	"security" => true,
 	"password" => "63a9f0ea7bb98050796b649e85481845",
 	"outputFolder" => "downloads",
 	"extracter" => "avconv",
