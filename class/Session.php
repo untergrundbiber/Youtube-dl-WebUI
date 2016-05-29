67626fc2f460520387643ba422cc7651c3fcaa0c
PiXELART <PixiBixi@users.noreply.github.com>
2016-05-29 11:45:45 +0200

    Fix class

--- class/Session.php
+++ class/Session.php
@@ -2,63 +2,53 @@
 
 class Session
 {
-	private $config = [];
+    private $config = [];
 
-	private static $_instance;
+    private static $_instance;
 
-	public function __construct()
-	{
-		session_start();
+    public function __construct()
+    {
+        session_start();
 
-		$this->config = require dirname(__DIR__).'/config/config.php';
+        $this->config = require dirname(__DIR__).'/config/config.php';
 
-		if($this->config["security"])
-		{
-			if(!isset($_SESSION["logged_in"]))
-			{
-				$_SESSION["logged_in"] = false;
-			}
-		}
-		else
-		{
-			$_SESSION["logged_in"] = true;
-		}
-	}
+        if ($this->config["security"]) {
+            if (!isset($_SESSION["logged_in"])) {
+                $_SESSION["logged_in"] = false;
+            }
+        } else {
+            $_SESSION["logged_in"] = true;
+        }
+    }
 
-	public static function getInstance()
-	{
-		if(is_null(self::$_instance))
-		{
-			self::$_instance = new Session();
-		}
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Session();
+        }
 
-		return self::$_instance;
-	}
+        return self::$_instance;
+    }
 
-	public function login($password)
-	{
-		if($this->config["password"] === md5($password))
-		{
-			$_SESSION["logged_in"] = true;
-			return true;
-		}
-		else
-		{
-			$_SESSION["logged_in"] = false;
-			return false;
-		}
-	}
+    public function login($password)
+    {
+        if ($this->config["password"] === md5($password)) {
+            $_SESSION["logged_in"] = true;
+            return true;
+        } else {
+            $_SESSION["logged_in"] = false;
+            return false;
+        }
+    }
 
-	public function is_logged_in()
-	{
-		return $_SESSION["logged_in"];
-	}
+    public function is_logged_in()
+    {
+        return $_SESSION["logged_in"];
+    }
 
-	public function logout()
-	{
-		$_SESSION = array();
-		session_destroy();
-	}
+    public function logout()
+    {
+        $_SESSION = array();
+        session_destroy();
+    }
 }
-
-?>
